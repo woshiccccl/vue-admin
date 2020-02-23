6ebb1a1ee1a3f1453069aa8a96ac30ca27a49e98
woshiccl <woshiccccl@163.com>
2020-02-23 10:32:18 +0800

    elemnetUI

---
+++ src/views/Login/index.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="login">    
+  <div class="login-wrap">
+     <ul> <li v-for="item in menuTab" :key="item.id" > {{item.txt}} </li> </ul>
+  </div>
+  </div>
+</template>
+<script>
+export default {
+  name: 'login',
+  data() {
+     return {
+        menuTab:[
+          {txt:'登录'},
+          {txt:'注册'}
+        ]
+     }
+  },
+}
+</script>
+<style lang="scss" scoped>
+#login {
+  height: 100vh;
+  background: #3445af;
+}
+.login-wrap{
+   margin: auto;
+   width: 330px
+}
+</style>
